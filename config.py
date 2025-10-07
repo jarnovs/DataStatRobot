0535078d273d3b0d7632a4f55c7077e9880650f2
Assol <zakirovaassol@gmail.com>
2025-10-07 16:05:07 +0600

    m

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 
-TOKEN = "7619172313:AAErRDJFJW8eoMU9GV1qZaEKP6qpAkt_iSY"
+TOKEN = "1234"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()  # In aiogram 3.x, Dispatcher is instantiated without bot
